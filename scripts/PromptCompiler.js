c4cde3fe660e945492d27ec3e40c69074b9dd8c9
Brett Lamy <brettlamy@gmail.com>
2023-04-01 18:47:04 -0700

    add static prompts and rail prompts

--- scripts/PromptCompiler.js
+++ scripts/PromptCompiler.js
@@ -9,19 +9,31 @@ module.exports = class PromptCompiler {
   }
 
   readPrompt(fileName) {
-    return fs.readFileSync(path.join(__dirname, this.promptsDirectory, fileName), "utf8");
+    return fs.readFileSync(
+      path.join(__dirname, this.promptsDirectory, fileName),
+      "utf8"
+    );
   }
 
   compileRailPrompt(fileName) {
     const railPath = path.join(__dirname, this.promptsDirectory, fileName);
-    let railProcess = cp.spawnSync("python", ["scripts/compileRailFile.py", railPath]);
+    let railProcess = cp.spawnSync("python", [
+      "scripts/compileRailFile.py",
+      railPath,
+    ]);
     if (railProcess.error) {
-      railProcess = cp.spawnSync("python3", ["scripts/compileRailFile.py", railPath]);
+      railProcess = cp.spawnSync("python3", [
+        "scripts/compileRailFile.py",
+        railPath,
+      ]);
       if (railProcess.error) {
-        throw new Error("Could not compile rail file. Make sure you have python is installed.");
+        throw new Error(
+          "Could not compile rail file. Make sure you have python is installed."
+        );
       }
     }
-    const compiledFile = railProcess.output.toString()
+    const compiledFile = railProcess.output
+      .toString()
       // gaurdrails kept wrapping the output in commas for some reason
       .slice(1, -1);
     return JSON.stringify(compiledFile);
@@ -34,8 +46,6 @@ module.exports = class PromptCompiler {
     const hasInput = file.includes("type Input =");
     const hasErrors = file.includes("type Errors =");
     const hasTools = file.includes("type Tools =");
-
-
 
     if (!hasPrompt || !hasOutput) {
       throw new Error("Prompt file must have both Prompt and Output types");
@@ -50,8 +60,8 @@ module.exports = class PromptCompiler {
     if (hasTools) {
       numTypes++;
     }
-    return "TODO"
-    // return 
+    return "TODO";
+    // return
     //   .replace(new RegExp("", "g"), "")
     //   .replace(new RegExp("", "g"), "");
   }
@@ -59,37 +69,50 @@ module.exports = class PromptCompiler {
   // Read all prompts from the prompts folder and add them to the DefinePlugin.
   // This allows us to get the uncompiled prompt files at runtime to send to GPT
   build() {
- // Read all prompts from the prompts folder and add them to the DefinePlugin.
-  // This allows us to get the uncompiled prompt files at runtime to send to GPT
-  const allPrompts = fs
-    .readdirSync(path.join(__dirname, this.promptsDirectory))
-    .filter((fileName) => {
-      const [_, ...extensions] = fileName.split(".");
-      return fileName !== "index.ts" && extensions.includes("Prompt");
-    });
+    // Read all prompts from the prompts folder and add them to the DefinePlugin.
+    // This allows us to get the uncompiled prompt files at runtime to send to GPT
+    const allPrompts = fs
+      .readdirSync(path.join(__dirname, this.promptsDirectory))
+      .filter((fileName) => {
+        const [_, ...extensions] = fileName.split(".");
+        return fileName !== "index.ts" && extensions.includes("Prompt");
+      });
 
-  const staticPrompts = allPrompts
-    .filter((fileName) => fileName.endsWith("Prompt"))
-    .reduce((acc, fileName) => ({ 
-      ...acc, 
-      [`process.env.${fileName.split(".")[0]}Prompt`]: JSON.stringify(this.readPrompt(fileName))
-    }), {});
+    const staticPrompts = allPrompts
+      .filter((fileName) => fileName.endsWith("Prompt"))
+      .reduce(
+        (acc, fileName) => ({
+          ...acc,
+          [`process.env.${fileName.split(".")[0]}Prompt`]: JSON.stringify(
+            this.readPrompt(fileName)
+          ),
+        }),
+        {}
+      );
 
-  const railPrompts = allPrompts
-    .filter((fileName) => fileName.endsWith("Prompt.rail"))
-    .reduce((acc, fileName) => ({ 
-      ...acc, 
-      [`process.env.${fileName.split(".")[0]}Prompt`]: this.compileRailPrompt(fileName) 
-    }), {});
+    const railPrompts = allPrompts
+      .filter((fileName) => fileName.endsWith("Prompt.rail"))
+      .reduce(
+        (acc, fileName) => ({
+          ...acc,
+          [`process.env.${fileName.split(".")[0]}Prompt`]:
+            this.compileRailPrompt(fileName),
+        }),
+        {}
+      );
 
-  const typescriptPrompts = allPrompts
-    .filter((fileName) => fileName.endsWith("Prompt.ts"))
-    .reduce((acc, fileName) => ({ 
-      ...acc, 
-      [`process.env.${fileName.split(".")[0]}Prompt`]: JSON.stringify(this.readPrompt(fileName)) 
-    }), {});
+    const typescriptPrompts = allPrompts
+      .filter((fileName) => fileName.endsWith("Prompt.ts"))
+      .reduce(
+        (acc, fileName) => ({
+          ...acc,
+          [`process.env.${fileName.split(".")[0]}Prompt`]: JSON.stringify(
+            this.readPrompt(fileName)
+          ),
+        }),
+        {}
+      );
 
     return { ...staticPrompts, ...railPrompts, ...typescriptPrompts };
   }
-}
-
+};
