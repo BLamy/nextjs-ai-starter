c5016d0d88c58bca7cb3ccfdbd69a8ae70a0a22e
Brett Lamy <brettlamy@gmail.com>
2023-05-03 02:32:32 +0000

    [Atom]: Searchbox - closes #25

---
+++ src/components/atoms/Search.tsx
@@ -0,0 +1,1 @@
+{id:chatcmpl-7BwZnmLFjbtckCVwXbxnnyu1Lhy5Q,object:chat.completion,created:1683081131,model:gpt-3.5-turbo-0301,usage:{prompt_tokens:133,completion_tokens:347,total_tokens:480},choices:[{message:{role:assistant,content:},finish_reason:stop,index:0}]}
