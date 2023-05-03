e6049ed1fa4e2dfd8670eb34d70e1d6b4c92a574
Brett Lamy <brettlamy@gmail.com>
2023-05-03 01:56:28 +0000

    Update file with API response

---
+++ src/components/atoms/Search.tsx
@@ -0,0 +1,1 @@
+{id:chatcmpl-7Bw13wbWF4eapgZhxMGHmhKfXM6QV,object:chat.completion,created:1683078977,model:gpt-3.5-turbo-0301,usage:{prompt_tokens:133,completion_tokens:176,total_tokens:309},choices:[{message:{role:assistant,content:},finish_reason:stop,index:0}]}
