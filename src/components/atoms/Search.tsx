65960682b03a967ad12694f064dab4bc6720935a
Brett Lamy <brettlamy@gmail.com>
2023-05-03 05:10:50 +0000

    [Atom]: SeaerchIcon - closes #68

---
+++ src/components/atoms/Search.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const SearchIcon: React.FC = () => (
+  <svg xmlns=http://www.w3.org/2000/svg viewBox=0 0 24 24 fill=currentColor>
+    <path d=M9.5 3C6.5 3 4 5.5 4 8.5S6.5 14 9.5 14 15 11.5 15 8.5 12.5 3 9.5 3zm10.3 18.7L18 18l1.7-1.7c-1-1.3-1.6-2.9-1.6-4.6 0-3.9 3.1-7 7-7s7 3.1 7 7-3.1 7-7 7c-1.7 0-3.3-.6-4.6-1.6zm1.3-3.1c1.5-1.9 2.4-4.2 2.4-6.6 0-5.5-4.5-10-10-10S3 4.5 3 10s4.5 10 10 10c2.4 0 4.7-.9 6.6-2.4l2.8 2.8 1.4-1.4-2.9-2.8z />
+  </svg>
+);
+
+export default SearchIcon;
+
