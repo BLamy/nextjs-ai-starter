a452fc573ab363c85a342d86c9e5feead440a640
BLamy <BLamy@users.noreply.github.com>
2023-05-10 22:57:15 +0000

    Button - closes #264

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,20 +1,60 @@
-import React, { MouseEventHandler } from "react";
+import React from "react";
 
 export type ButtonProps = {
-  text: string;
-  onClick: MouseEventHandler<HTMLButtonElement>;
-  className?: string;
+  color?:
+    | "red"
+    | "blue"
+    | "green"
+    | "yellow"
+    | "orange"
+    | "teal"
+    | "purple"
+    | "pink";
+  size?: "sm" | "md" | "lg";
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  disabled?: boolean;
 };
 
-const Button: React.FC<ButtonProps> = ({ text, onClick, className }) => {
+const Button: React.FC<ButtonProps> = ({
+  color = "blue",
+  size = "md",
+  onClick,
+  disabled,
+  children,
+}) => {
+  const sizeClass = {
+    sm: "py-1.5 px-2",
+    md: "py-2 px-4",
+    lg: "py-3 px-5",
+  };
+
+  const colorClass = {
+    red: "bg-red-500 hover:bg-red-600 focus:bg-red-700 active:bg-red-800 text-white",
+    blue: "bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 active:bg-blue-800 text-white",
+    green:
+      "bg-green-500 hover:bg-green-600 focus:bg-green-700 active:bg-green-800 text-white",
+    yellow:
+      "bg-yellow-500 hover:bg-yellow-600 focus:bg-yellow-700 active:bg-yellow-800 text-white",
+    orange:
+      "bg-orange-500 hover:bg-orange-600 focus:bg-orange-700 active:bg-orange-800 text-white",
+    teal: "bg-teal-500 hover:bg-teal-600 focus:bg-teal-700 active:bg-teal-800 text-white",
+    purple:
+      "bg-purple-500 hover:bg-purple-600 focus:bg-purple-700 active:bg-purple-800 text-white",
+    pink: "bg-pink-500 hover:bg-pink-600 focus:bg-pink-700 active:bg-pink-800 text-white",
+  };
+
   return (
     <button
+      className={`inline-block rounded text-center ${sizeClass[size]} ${
+        colorClass[color]
+      } ${disabled ? "opacity-50 cursor-not-allowed" : ""}`}
       onClick={onClick}
-      className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ${className}`}
+      disabled={disabled}
     >
-      {text}
+      {children}
     </button>
   );
 };
 
 export default Button;
+export type { ButtonProps };
--- src/components/atoms/__tests__/Button.stories.tsx
+++ src/components/atoms/__tests__/Button.stories.tsx
@@ -1,23 +1,47 @@
 import React from "react";
-import { Story, Meta } from "@storybook/react/types-6-0";
-
+import { Story, Meta } from "@storybook/react";
 import Button, { ButtonProps } from "../Button";
 
 export default {
   title: "Example/Button",
   component: Button,
   argTypes: {
-    text: {
-      description: "The text to display",
-      defaultValue: "Button",
-      control: "text",
+    color: {
+      control: "select",
+      options: [
+        "red",
+        "blue",
+        "green",
+        "yellow",
+        "orange",
+        "teal",
+        "purple",
+        "pink",
+      ],
     },
+    size: { control: "select", options: ["sm", "md", "lg"] },
+    disabled: { control: "boolean" },
+    onClick: { action: "clicked" },
   },
 } as Meta;
 
 const Template: Story<ButtonProps> = (args) => <Button {...args} />;
 
-export const DefaultButton = Template.bind({});
-DefaultButton.args = {
-  text: "Click Me!",
+export const Default = Template.bind({});
+Default.args = {
+  children: "Button",
+};
+
+export const RedLarge = Template.bind({});
+RedLarge.args = {
+  color: "red",
+  size: "lg",
+  children: "Button",
+};
+
+export const YellowDisabled = Template.bind({});
+YellowDisabled.args = {
+  color: "yellow",
+  disabled: true,
+  children: "Button",
 };
