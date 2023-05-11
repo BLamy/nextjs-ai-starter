c3a5dca4341e29af2432149ecde745d6ed7d4205
Brett Lamy <brettlamy@gmail.com>
2023-05-11 00:41:40 -0700

    Update page.tsx

--- src/app/rail/page.tsx
+++ src/app/rail/page.tsx
@@ -1,14 +1,15 @@
 import { ChatCompletionRequestMessage } from "openai";
 import { generateChatCompletion, user, assistant } from "@/lib/ChatCompletion";
 import Chat from "@/components/organisms/Chat";
-import Prompts from "@/ai/prompts";
+// import Prompts from "@/ai/prompts";
 
 export default async function RailExample() {
-  const messages: ChatCompletionRequestMessage[] = [user(Prompts.BankRun)];
-  const res = await generateChatCompletion(messages, {
-    model: "gpt-4",
-  });
-  return (
-    <Chat messages={[...messages, assistant(JSON.stringify(res, null, 2))]} />
-  );
+  return <Chat messages={[user("TODO"), assistant("FIX ME")]} />;
+  // const messages: ChatCompletionRequestMessage[] = [user(Prompts.BankRun)];
+  // const res = await generateChatCompletion(messages, {
+  //   model: "gpt-4",
+  // });
+  // return (
+  //   <Chat messages={[...messages, assistant(JSON.stringify(res, null, 2))]} />
+  // );
 }
